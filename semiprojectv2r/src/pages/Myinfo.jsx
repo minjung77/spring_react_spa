7eaa7ce52cce6f3945c6dc84caddedf3fa5c40fb
zzyzzy <zzyzigy@gmail.com>
2025-03-18 12:47:48 +0900

    Myinfo 출력

--- semiprojectv2r/src/pages/Myinfo.jsx
+++ semiprojectv2r/src/pages/Myinfo.jsx
@@ -4,7 +4,34 @@ import React from "react";
 const Myinfo = () => {
     return (
         <main id="content">
-            <img src="image/spyfamily.png" className="img-fluid" />
+            <h2>회원정보</h2>
+            <div id="myinfo">
+                <table className="table table-bordered mb-0">
+                    <colgroup>
+                        <col style={{width: "30%"}} />
+                        <col/>
+                    </colgroup>
+                    <tbody>
+                    <tr>
+                        <td>아이디</td>
+                        <td><span>abc123</span></td>
+                    </tr>
+                    <tr>
+                        <td>이름</td>
+                        <td><span>abc123</span></td>
+                    </tr>
+                    <tr>
+                        <td>이메일</td>
+                        <td><span>abc123@abc123.co.kr</span></td>
+                    </tr>
+                    <tr>
+                        <td>가입일</td>
+                        <td><span>2025-03-18 12:35:35</span>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </main>
     )
 }
