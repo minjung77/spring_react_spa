8fb2f451b42ed7094707afd749b97e351eff5c7a
zzyzzy <zzyzigy@gmail.com>
2025-03-18 10:12:29 +0900

    Initial commit

---
+++ SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/SemiProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.example.zzyzzy.semiprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProjectV2Application.class, args);
+	}
+
+}
