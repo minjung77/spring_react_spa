8f524fac4c2c753a3fe558019c072cd5184572cd
zzyzzy <zzyzigy@gmail.com>
2025-03-19 10:37:07 +0900

    회원가입시 폼 재설정 기능 추가

--- semiprojectv2r/src/pages/Join.jsx
+++ semiprojectv2r/src/pages/Join.jsx
@@ -1,13 +1,24 @@
 import {useRef, useState} from "react";
 import "../styles/member.css";
 
+// 폼 재설정 함수 - 외부로 빼냄
+const resetForm = (formJoinRef, setErrors) => {
+    formJoinRef.current.reset();
+    setErrors({});
+}
+
 // Join 함수 컴포넌트 정의
 const Join = () => {
     // form 요소 참조를 위한 ref 변수 생성
     const formJoinRef = useRef(null);
 
     // 오류 상태를 위한 변수 선언
+    // errors : 상태를 저장하기 위한 변수
+    // setErrors : errors 변수의 상태를 변경하는 함수
     const [errors, setErrors] = useState({});
+
+    // 폼 재설정 처리
+    const handleReset = () => resetForm(formJoinRef, setErrors);
 
     // 폼 제출시 데이터 처리 및 유효성 검사
     const handleJoinSubmit = (e) => {
@@ -124,7 +135,7 @@ const Join = () => {
                     <button type="submit" className="btn btn-primary">
                         <i className="fa-sharp fa-solid fa-file-signature"></i> 입력완료
                     </button>
-                    <button type="reset" className="btn btn-danger">
+                    <button type="button" className="btn btn-danger" onClick={handleReset}>
                         <i className="fa-sharp fa-solid fa-eraser"></i> 다시입력
                     </button>
                 </div>
