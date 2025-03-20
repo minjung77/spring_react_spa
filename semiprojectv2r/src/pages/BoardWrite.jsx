62daab6ded1ff628f2ff966334a46be5d0b71859
min <fheka77@gmail.com>
2025-03-20 16:29:02 +0900

    processBoardok 구현

--- semiprojectv2r/src/pages/BoardWrite.jsx
+++ semiprojectv2r/src/pages/BoardWrite.jsx
@@ -1,27 +1,83 @@
-import React from "react";
+import {useRef, useState} from "react";
 import "../styles/board.css"
+
+
+const validateBoardForm = (values) => {
+    let formErrors = {};
+    if(!values.userid) {
+        formErrors.userid = '아이디를 입력하세요'
+    }else if(values.userid.length < 6){
+        formErrors.userid = '아이디는 6자 이상이어야 합니다.';
+    }
+
+    if(!values.title){formErrors.title = '타이틀을 입력하세요'}
+    if(!values.contents){formErrors.contents = '내용을 입력하세요'}
+
+    return formErrors;
+}
+const processBoardok = async (formValues) => {
+    fetch('https://localhost:8080/api/board/write', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formValues)
+    }).then(async response => {
+        if(response.ok){
+            alert('글쓰기가 완료되었습니다');
+            location.href="/board/list";
+        }else if(response.status === 400){
+            alert(await response.text());
+        }else {
+            alert('게시판 글쓰기에 실패했습니다. 다시 시도해주세요')
+        }
+    }).catch(errors =>{
+        console.log(errors);
+        alert('서버와 통신중 오류가 발생했습니다. 관리자에게 문의하세요.');
+    })
+}
 
 // BoardList 함수 컴포넌트 정의
 const BoardWrite = () => {
+
+    const formBoardRef = useRef(null);
+    const [errors, setErrors] = useState({});
+    const handleBoardSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(formBoardRef.current);
+        const formValues = Object.fromEntries(formData.entries());
+
+        const formErrors = validateBoardForm(formValues);
+
+        if(Object.keys(formErrors).length === 0){
+            processBoardok(formValues);
+        }else {
+            setErrors(formErrors);
+            alert('등록 실패');
+        }
+    }
+
+
     return (
         <main id="content">
             <h2>게시판 글쓰기</h2>
-            <form method="post" name="boardfrm" id="boardfrm" noValidate>
+            <form method="post" name="boardfrm" id="boardfrm" ref={formBoardRef} onSubmit={handleBoardSubmit} noValidate>
                 <div className="form-floating my-2">
-                    <input type="text" name="userid" id="userid" className="form-control" placeholder="아이디" readOnly
+                    <input type="text" name="userid" id="userid" className={`form-control ${errors.userid ? 'is-invalid':''}`} placeholder="아이디" readOnly
                            value="abc123"/>
                     <label htmlFor="userid" form-label>아이디</label>
+                    {errors.userid && <div className="invalid-feedback">{errors.userid}</div>}
                 </div>
 
-
                 <div className="form-floating my-2">
-                    <input type="text" name="title" className="form-control" id="title" placeholder="제목" required/>
+                    <input type="text" name="title" className={`form-control ${errors.title ? 'is-invalid':''}`} id="title" placeholder="제목" required/>
                     <label htmlFor="title">제목</label>
+                    {errors.title && <div className="invalid-feedback">{errors.title}</div>}
                 </div>
                 <div className="form-floating my-2">
-                    <textarea name="contents" className="form-control h-100" id="contents" rows="10" placeholder="본문글"
+                    <textarea name="contents" className={`form-control h-100 ${errors.contents?'is-invalid':''}`} id="contents" rows="10" placeholder="본문글"
                               required></textarea>
                     <label htmlFor="contents">본문글</label>
+                    {errors.contents && <div className="invalid-feedback">{errors.contents}</div>}
                 </div>
 
                 <div className="my-2 d-flex justify-content-center">
