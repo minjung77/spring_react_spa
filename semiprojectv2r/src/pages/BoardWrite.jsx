9d91ad48b2e83568801696b84210f0e6bc6c63e3
min <fheka77@gmail.com>
2025-03-20 15:18:32 +0900

    boardList, BoardWrite 출력

---
+++ semiprojectv2r/src/pages/BoardWrite.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "../styles/board.css"
+
+// BoardList 함수 컴포넌트 정의
+const BoardWrite = () => {
+    return (
+        <main id="content">
+            <h2>게시판 글쓰기</h2>
+            <form method="post" name="boardfrm" id="boardfrm" noValidate>
+                <div className="form-floating my-2">
+                    <input type="text" name="userid" id="userid" className="form-control" placeholder="아이디" readOnly
+                           value="abc123"/>
+                    <label htmlFor="userid" form-label>아이디</label>
+                </div>
+
+
+                <div className="form-floating my-2">
+                    <input type="text" name="title" className="form-control" id="title" placeholder="제목" required/>
+                    <label htmlFor="title">제목</label>
+                </div>
+                <div className="form-floating my-2">
+                    <textarea name="contents" className="form-control h-100" id="contents" rows="10" placeholder="본문글"
+                              required></textarea>
+                    <label htmlFor="contents">본문글</label>
+                </div>
+
+                <div className="my-2 d-flex justify-content-center">
+                    <div className="g-recaptcha" id="recaptcha" th:data-sitekey=""></div>
+                </div>
+
+                <div className="my-2 d-flex justify-content-between">
+                    <button type="submit" className="btn btn-primary">
+                        <i className="fa-sharp fa-solid fa-file-signature"></i> 입력완료
+                    </button>
+                    <button type="reset" className="btn btn-danger">
+                        <i className="fa-sharp fa-solid fa-eraser"></i> 다시입력
+                    </button>
+                </div>
+            </form>
+        </main>
+    )
+}
+
+export default BoardWrite;
