3a17bcea923524dd6db93a26911a9da770807831
min <fheka77@gmail.com>
2025-03-28 11:35:52 +0900

    리액트 JSX로 댓글/대댓글 쓰기 폼 출력

--- semiprojectv2r/src/pages/BoardView.jsx
+++ semiprojectv2r/src/pages/BoardView.jsx
@@ -87,20 +87,42 @@ const BoardView = () => {
                         <h3><i className="fa fa-commenting">나도 한 마디</i></h3>
                     </div>
                     <table className="table reply-border" data-loginuser="">
+                        <form className="card card-body bg-light" method="post"
+                              name="replyfrm" id="replyfrm">
+                            <div className="row align-items-center">
+                                <div className="col text-center"><label>abc123</label></div>
+
+                                <div className="col-8"><textarea id="comments" rows="10" name="comments"
+                                                                 className="form-control col-7"></textarea></div>
+
+                                <div className="col">
+                                    <button type="button" id="replybtn"
+                                            className="btn btn-dark">
+                                        <i className="fa fa-commenting"></i>댓글쓰기
+                                    </button>
+                                </div>
+                            </div>
+                            <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+                            <input type="hidden" name="userid" value="abc123"/>
+                            <input type="hidden" name="pno" value={bno}/>
+                        </form>
                         <tbody>
                         {
                             (boardData.rps) && boardData.rps.map(rp => (
                                 (rp.rno === rp.ref) ?
                                     <>
-                                        (<tr key={`reply-${rp.rno}`}>
+                                        (
+                                        <tr key={`reply-${rp.rno}`}>
                                             <td className="text-left">{rp.userid}</td>
                                             <td>
                                                 <div className="cmtbg"><span>{rp.regdate}</span>
                                                     <span className="pushend"><a href="#" data-ref="${rp.rno}"
-                                                      data-bs-toggle="modal" data-bs-target="#cmtModal">[추가]</a> [수정] [삭제]</span>
+                                                                                 data-bs-toggle="modal"
+                                                                                 data-bs-target="#cmtModal">[추가]</a> [수정] [삭제]</span>
                                                 </div>
                                                 <p className="py-1 pre">{rp.comments}</p></td>
-                                        </tr>)
+                                        </tr>
+                                        )
                                     </>
                                     :
                                     (<tr key={`reply-${rp.rno}`}>
@@ -110,13 +132,38 @@ const BoardView = () => {
                                                 <span className=" pushend">{rp.regdate}</span></div>
                                             <p className="py-1 pre">{rp.comments}</p>
                                         </td>
-                                   </tr>)
+                                    </tr>)
                             ))
                         }
                         </tbody>
                     </table>
                 </div>
+                <div className="modal hide" id="cmtModal" role="dialog">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h3 className="modal-title">대댓글 쓰기</h3>
+                            </div>
+                            <div className="modal-body">
+                                <form name="cmmtfrm" id="cmmtfrm" method="post">
+                            <textarea name="comments" id="cmmt" rows="8" cols="60"
+                                      className="span4"></textarea>
+                                    <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+                                    <input type="hidden" name="userid" value="abc123"/>
+                                    <input type="hidden" name="pno" value={bno}/>
+                                    <input type="hidden" name="ref" id="refno"/>
+                                </form>
+                            </div>
+                            <div className="modal-footer justify-content-center">
+                                <button type="button" id="cmmtbtn"
+                                        className="btn btn-danger">대댓글 작성
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main>
+
         </>
     );
 }
