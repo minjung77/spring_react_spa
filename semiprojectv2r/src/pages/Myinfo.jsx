a132a8df672eaa37aa35d34bdc6715ac88da609d
min <fheka77@gmail.com>
2025-04-02 10:17:43 +0900

    Myinfo에 조건부 렌더링 적용

--- semiprojectv2r/src/pages/Myinfo.jsx
+++ semiprojectv2r/src/pages/Myinfo.jsx
@@ -6,18 +6,19 @@ import React, {useEffect, useState} from "react";
 const Myinfo = () => {
 
     const [userInfo, setUserInfo] = useState({});
+    const [loading, setLoading] = useState(true);
     const fetchURL = 'http://localhost:8080/api/member/myinfo';
 
    useEffect(() => {
-       const token = localStorage.getItem('token');
+       const token = localStorage.getItem('accessToken');
        const headers = {};
        headers['Content-Type'] = 'application/json';
        headers['Accept'] = 'application/json';
-       
+
        //토큰이 존재하면 인증 헤더에 토큰을 설정하고
        if(token != null) headers['Authorization'] = `Bearer ${token}`
        // 토큰이 없으면 로그인 페이지로 이동
-       else location.href = "member/login";
+       else location.href = "/member/login";
 
        fetch(fetchURL, {
            headers: headers,
@@ -26,13 +27,18 @@ const Myinfo = () => {
        .then(data => {
            console.log(data);
            setUserInfo(data);
+           setLoading(false);// 조건부 렌더링
        })
        .catch(err => {
            console.log('오류발생!! ', err);
-           location.href = "member/login";
+           location.href = "/member/login";
        });
    }, []);
 
+    // 조건부 렌더링
+    if (loading) {
+        return (<></>)
+    }
     return (
         <main id="content">
             <h2>회원정보</h2>
