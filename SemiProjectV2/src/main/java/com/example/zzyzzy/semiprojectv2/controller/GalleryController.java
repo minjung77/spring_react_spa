2eeaccace5fe6228d80298677e7f4c7643b1cef8
min <fheka77@gmail.com>
2025-04-04 10:44:47 +0900

    갤러리 구현을 위한 컨트롤러 클래스 구현

---
+++ SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/controller/GalleryController.java
@@ -0,0 +1,53 @@
+package com.example.zzyzzy.semiprojectv2.controller;
+
+import com.example.zzyzzy.semiprojectv2.domain.Gallery;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/gallery")
+@RequiredArgsConstructor
+public class GalleryController {
+//    Query String (질의문자열)
+//    URL의 ? 뒤에 key=value 형태로 데이터를 전달하는 방식
+//    ex) /users?name=John&age=30에서 name과 age가 Query String 매개변수
+//    스프링 부트에서는 @RequestParam
+//    어노테이션을 사용하여 처리
+//    검색 조건, 필터링, 정렬 등 복잡한 데이터를 전달하기에 적합
+//
+//    Path Variable (경로 변수)
+//    URL 경로 자체에 데이터를 포함시키는 방식
+//    ex) /users/John/30에서 John, 30이 Path Variable로 사용
+//    스프링 부트에서는 @PathVariable
+//    어노테이션을 사용하여 처리
+//    RESTful API 설계에서 자원의 식별자로 사용하기에 적합
+
+    @PostMapping("/write")
+    public ResponseEntity<?> writeok(Gallery gal, List<MultipartFile> ginames, @RequestParam("g-recaptcha-response") String gRrecaptchaResponse) {
+        ResponseEntity<?> response = ResponseEntity.internalServerError().build();
+        log.info("submit 된 겔러리 정보1 ::: {}", gal);
+        log.info("submit 된 갤러리 정보2 ::: {}", ginames);
+
+        try{
+            if(!googleRecaptchaService.verifyRecaptcha(gRrecaptchaResponse)){
+                throw new IllegalStateException("자동가입방지 코드 오류!!!");
+            }
+
+            if(galleryService.newGalleryImage(gal, ginames)){
+                response = ResponseEntity.ok().build();
+            }
+        }catch (IllegalStateException e){
+            response = ResponseEntity.badRequest().body(e.getMessage());
+        }finally{
+        }
+        return response;
+    }
+}
