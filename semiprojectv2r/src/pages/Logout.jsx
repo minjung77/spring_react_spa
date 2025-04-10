ed1f66644d868f18432709f3a4ab42aa8e4b5ba4
min <fheka77@gmail.com>
2025-04-10 10:39:47 +0900

    카카오 로그인에서 로그아웃 구현 2 - 완전한 로그아웃 구현 (리액트)

--- semiprojectv2r/src/pages/Logout.jsx
+++ semiprojectv2r/src/pages/Logout.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 const Logout = () => {
     // localStorage.clear()
 
-    if (localStorage.getItem("kakao")) {
-        localStorage.removeItem("kakao");
+    const kakaoLogout = async () => {
+        const response1 = await fetch('https://kapi.kakao.com/v1/user/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+                'Authorization': `Bearer ${localStorage.getItem('accesstoken')}`,
+            }
+        });
+        if(response1.ok){
+            throw new Error(`오류발생 ${response1.statusText}`);
+        }
+        const data1 = response1.json();
+        console.log(data1);
+
+        const logoutUrl = 'http://kauth.kakao.com/oauth/logout';
+        const params = `?client_id=${import.meta.env.VITE_APP_KAKAO_API_KEY}&logout_redirect_uri=${import.meta.env.VITE_APP_KAKAO_LOGOUT_URI}`;
+
+        window.location.href=logoutUrl+params;
     }
 
-    localStorage.removeItem("accessToken");
+    useEffect(() =>{
+        const kakao = localStorage.getItem("kakao");
+
+        if (kakao) {
+            kakaoLogout();
+            localStorage.removeItem("kakao");
+        }
+        localStorage.removeItem("accessToken");
+    },[]);
 
     return (
-        location.href="/"
+        <></>
     );
 }
 export default Logout;
